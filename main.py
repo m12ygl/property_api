75e029bb103ec829386c10a510ad67b261840399
m12ygl <ifrahyohann100@gmail.com>
2025-03-28 09:43:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ async def view_source(town: str = Query(...)):
             await browser.close()
             return {"html": content}
     except Exception as e:
-        return {"error": str(e)}}
+        return {"error": str(e)}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=10000)
